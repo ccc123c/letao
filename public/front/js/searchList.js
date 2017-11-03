06081acd291669abf68ce5ac7c372a58c40df3ef
cheri <18862005736@163.com>
2017-11-03 16:12:48 +0800

    商品排序功能完成

--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -7,26 +7,36 @@ $(function(){
     })
     var currentPage=1;
     var pageSize=10;
+    var data={
+        proName:'',
+        brandId:'',
+        price:'',
+        num:'',
+        page:currentPage,
+        pageSize:pageSize
+    }
+    function render(data){
+        $.ajax({
+            type:"get",
+            url:"/product/queryProduct",
+            data:data,
+            success:function(data){
+                setTimeout(function(){
+                    $(".lt_product ul").html(template("tpl",data));
+                },1000);
+            }
+        })
+    }
     /*获取地址栏的参数设置到文本框*/
     var key=tools.getValue("key");
     $('.lt_search .search_text').val(key);
 
 
     /*需要发送ajax请求，去后端获取数据*/
-    $.ajax({
-        type:"get",
-        url:"/product/queryProduct",
-        data:{
-            proName:key,
-            page: currentPage,
-            pageSize: pageSize
-        },
-        success:function(data){
-            setTimeout(function(){
-                $(".lt_product ul").html(template("tpl",data));
-            },1000);
-        }
-    })
+    data.proName=key;
+   /* []语法表示属性名的时候要用引号，表示变量的时候不用引号
+    data["proName"]=key;*/
+    render(data);
 
 
   /*点击搜索按钮*/
@@ -35,24 +45,36 @@ $(function(){
         if(key===""){
             mui.alert("请输入要搜索的内容","温馨提示");
         }
+        /*需要保证所有的now都清掉，并且所有的箭头都向下*/
+        $(".lt_sort a").removeClass("now");
+        $(".lt_sort span").removeClass("fa-angle-up").addClass("fa-angle-down");
+        data.price='';
+        data.num='';
         /*这一步是用来当点击搜索按钮还能重新显示loading的那个小方块*/
         $('.lt_product ul').html('<div class="loading"></div>');
-        $.ajax({
-            type:"get",
-            url:"/product/queryProduct",
-            data:{
-                proName:key,
-                page: currentPage,
-                pageSize: pageSize
-            },
-            success:function(data){
-              setTimeout(function(){
-                  $(".lt_product ul").html(template("tpl",data));
+       data.proName=key;
+        render(data);
+    })
 
-              },1000);
 
-            }
-        })
-
+    /*排序功能*/
+    $('.lt_sort>a[data-type]').on('click',function(){
+        /*如果当前有now  只要切换箭头类型*/
+        /*如果当前没有now 给当前a标签加上now，并且清除其他标签的now类和将所有箭头向下*/
+        var $this=$(this);
+        var $span=$this.find("span");
+        if($this.hasClass('now')){
+            $span.toggleClass("fa-angle-down").toggleClass("fa-angle-up");
+        }else{
+            $this.addClass("now").siblings().removeClass("now");
+            $('.lt_sort a').find("span").removeClass("fa-angle-up").addClass("fa-angle-down");
+        }
+        var type=$this.data("type");
+        var value=$span.hasClass("fa-angle-down")?2:1;
+        //设置num或者price ，在这之前需要保证之前的清空
+        data.price='';
+        data.num='';
+        data[type]=value;
+        render(data);
     })
 });
